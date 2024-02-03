4514ef777b438770d01319565e31ff554ba0dead
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2024-02-02 23:07:40 -0500

    Add file upload functionality and histogram visualization

--- app.jl
+++ app.jl
@@ -1,8 +1,64 @@
 using GenieFramework, DataFrames, CSV, PlotlyBase
 @genietools
 
+
+const FILE_PATH = joinpath("public", "uploads")
+mkpath(FILE_PATH)
+
 @app begin   
 # Logic goes here
+    
+    @in selected_file = ""
+    @in selected_column = ""
+    @out columns = []
+    @out upfiles = readdir(FILE_PATH)
+    @out trace = [histogram()]
+    @out layout = PlotlyBase.Layout(yaxis_title_text="Count",xaxis_title_text="Value")
+    @private data = DataFrame()
+
+    @onchange fileuploads begin
+        if ! isempty(fileuploads)
+            @info "File was uploaded: " fileuploads
+            filename = fileuploads["name"]
+            println(fileuploads["path"])
+            ## Temporary workaround for windows error only!
+            tempPath =  joinpath(splitdir(tempname())[1], split(fileuploads["path"], "Temp")[2])
+            println(tempPath)
+            try
+                isdir(FILE_PATH) || mkpath(FILE_PATH)
+                mv(tempPath, joinpath(FILE_PATH, filename), force=true)
+            catch e
+                @error "Error processing file: $e"
+                notify(__model__,"Error processing file: $(fileuploads["name"])")
+            end
+
+            fileuploads = Dict{AbstractString,AbstractString}()
+        end
+        upfiles = readdir(FILE_PATH)
+    end
+    @event uploaded begin
+        @info "uploaded"
+        notify(__model__, "File was uploaded")
+    end
+    @event rejected begin
+        @info "rejected"
+        notify(__model__, "Please upload a valid file")
+    end
+
+    @onchange isready,selected_file begin
+        if ! isempty(selected_file)
+            @info "Selected file: $selected_file"
+            data = CSV.read(joinpath(FILE_PATH, selected_file), DataFrame)
+            columns = names(data)
+        end
+    end
+
+    @onchange isready, selected_column begin
+        if ! isempty(selected_column)
+            trace = [histogram(x=data[!, selected_column])]
+        end
+    end
+
 end
 
 @page("/", "ui.jl")
---
+++ ui.jl
@@ -0,0 +1,46 @@
+heading("CSV Analysis")
+ row([
+      cell(class="col-md-12", 
+          [
+          uploader( multiple = true,
+              accept = ".csv",
+              maxfilesize = 1024*1024*1, # bytes
+              maxfiles = 3,
+              autoupload = true,
+              hideuploadbtn = false,
+              label = "Upload datasets",
+              nothumbnails = true,
+              style="max-width: 95%; width: 95%; margin: 0 auto;",
+              @on("rejected", :rejected),
+              @on("uploaded", :uploaded)
+              )
+
+          ])
+ ])
+
+ row([
+     cell(
+          class="st-module",
+          [
+           h6("File")
+           Stipple.select(:selected_file; options=:upfiles)
+          ]
+         )
+     cell(
+          class="st-module",
+          [
+           h6("Column")
+           Stipple.select(:selected_column; options=:columns)
+          ]
+         )
+    ])
+
+row([
+    cell(
+            class="st-module",
+            [
+            h5("Histogram")
+            plot(:trace, layout=:layout)
+            ]
+        )
+    ])
